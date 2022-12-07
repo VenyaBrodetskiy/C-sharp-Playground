5b528fd4799a4f8a6b752c8e1d6ac860a52de175
Venya Brodetskiy <benjamincheg@gmail.com>
2022-12-07 22:00:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿internal class Program
+{   
+    public static int myMultiply(int a, int b)
+    {
+        return a * b;
+    }
+    private static void Main(string[] args)
+    {
+        PrintToConsole Printer = new PrintToConsole();
+        MultiBoard board = new(MultiBoard.Multiply, Printer);
+
+        string message = 
+            $"Hello, this is board for operation: {nameof(MultiBoard.Multiply)}\n" +
+            $"Printing board with: {nameof(PrintToConsole)}";
+
+        board.CalculateAndPrint(message);
+    }
+}
+
+internal class MultiBoard
+{
+    public int width;
+    public int height;
+    public Func<int, int, int> Operation;
+    public IPrint Printer;
+
+
+    public MultiBoard(Func<int, int, int> Operation, IPrint Printer, int width = 9, int height = 9)
+    {
+        this.width = width;
+        this.height = height; 
+        this.Operation = Operation;
+        this.Printer = Printer;
+    }
+
+    public void CalculateAndPrint(string message)
+    {
+        Printer.Write(message);
+        Printer.NewLine();
+
+        for (int i = 1; i <= width; i++)
+        {
+            for (int j = 1; j <= height; j++)
+            {
+                Printer.Write($"{(this.Operation(i, j)).ToString().PadLeft(2)} ");
+            }
+            Printer.NewLine();
+        }
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+interface IPrint
+{
+    void Write(string s);
+    void NewLine();
+}
+internal class PrintToConsole : IPrint
+{
+
+    public void Write(string s)
+    {
+        Console.Write(s);
+    }
+
+    public void NewLine()
+    {
+        Console.WriteLine();
+    }
+}
+
